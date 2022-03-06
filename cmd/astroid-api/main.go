d5a77198b12a2298611fef3e5d918816aaba98de
Valerius Mattfeld <mail@valerius.me>
2022-03-06 11:31:37 +0100

    orbit db rest response

--- cmd/astroid-api/main.go
+++ cmd/astroid-api/main.go
@@ -3,29 +3,34 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
 	odb "github.com/pastoapp/astroid-api/internal/orbitdb"
+	"github.com/pastoapp/astroid-api/internal/routes"
 )
 
 func main() {
-	//r := gin.Default()
-	//r.GET("/ping", func(c *gin.Context) {
-	//	c.JSON(200, gin.H{
-	//		"message": "pong",
-	//	})
-	//})
-	//
-	//// init modules
-	//routes.InitUsers(r)
-	//routes.InitMessages(r)
-	//routes.InitAuth(r)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
-	//r.Run(":3000")
+	// init modules
+	routes.InitUsers(r)
+	routes.InitMessages(r)
+	routes.InitAuth(r)
+
 	store, err := odb.CreateStore("test")
+
 	if err != nil {
 		panic(err)
 	}
 	defer store.Close()
+	routes.InitDatabases(r, store)
 
-	fmt.Printf("%+v\n", store)
+	fmt.Printf("%+v\n", store.Address())
+
+	r.Run(":3000")
 
 }
--- internal/routes/databases.go
+++ internal/routes/databases.go
@@ -1,1 +1,50 @@
 package routes
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"berty.tech/go-orbit-db/address"
+	"berty.tech/go-orbit-db/iface"
+	"github.com/gin-gonic/gin"
+	"github.com/pastoapp/astroid-api/internal/orbitdb"
+)
+
+var odb = orbitdb.OrbitDB
+
+type Database struct {
+	// __id      string
+	name      string
+	address   address.Address
+	storeType string
+}
+
+func InitDatabases(router *gin.Engine, store iface.Store) {
+	log.Println("Test databases")
+	databases := router.Group("/databases")
+	databases.GET("/", Database{
+		name:      store.DBName(),
+		address:   store.Address(),
+		storeType: store.Type(),
+	}.findAll)
+	// databases.POST("/", Database{}.create)
+}
+
+func (db Database) findAll(c *gin.Context) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	store, err := odb.Open(ctx, db.address.String(), nil)
+
+	if err != nil {
+		log.Fatalf("Error opening store: %s", err)
+	}
+
+	c.JSON(200, gin.H{
+		"dbData": fmt.Sprintf("%+v", store),
+	})
+}
+
+func (db Database) create(c *gin.Context) {
+
+}
